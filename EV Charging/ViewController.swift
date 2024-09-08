6b52199f37229b9e785f547c82e4e66386ba55b6
Olivier Miserez <olivier.miserez@me.com>
2024-09-08 11:52:51 +0200

    Initial Commit

---
+++ EV Charging/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EV Charging
+//
+//  Created by Olivier Miserez on 08/09/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
