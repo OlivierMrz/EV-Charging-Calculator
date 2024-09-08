bdd6127cc3ed85eb8e1e46174f64050bd03db1d0
Olivier Miserez <olivier.miserez@me.com>
2024-09-08 15:17:19 +0200

    Inital commit

---
+++ EV Charging/extensions/Colors.swift
@@ -0,0 +1,16 @@
+//
+//  Colors.swift
+//  EV Charging
+//
+//  Created by Olivier Miserez on 08/09/2024.
+//
+
+import UIKit
+
+class Colors {
+    static let black = UIColor(red: 27/255, green: 27/255, blue: 27/255, alpha: 1.0)
+    static let blue = UIColor(red: 65/255, green: 150/255, blue: 255/255, alpha: 1.0)
+    static let green = UIColor(red: 98/255, green: 250/255, blue: 129/255, alpha: 1.0)
+    static let background = UIColor(red: 251/255, green: 251/255, blue: 251/255, alpha: 1.0)
+    static let white = UIColor(red: 255/255, green: 255/255, blue: 255/255, alpha: 1.0)
+}
